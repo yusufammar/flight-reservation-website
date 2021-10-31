718269f8f8e6a7d83fc7769e1efd73cedc7c0aeb
Yousif Ammar <yousifammar.ya@gmail.com>
2021-10-31 20:33:58 +0200

    fixed database & CONNECTION TO IT

--- src/Models/Flights.js
+++ src/Models/Flights.js
@@ -25,5 +25,7 @@ const FlightsSchema = new Schema({
  
 }, { timestamps: true }, { collection: 'Flights' });
 
-const Flights = mongoose.model('Flights', FlightsSchema);
-module.exports = Flights;
+const Flight = mongoose.model('flights', FlightsSchema);   
+// first paramter is the collection name where the document/entry will be added it to || Note: the collection name must be in lowercase letters (if you entered uppercase letters the will changed to be all lowercase letters, & if it didn't find the collection specified it will create a new collection)
+//2nd paramater is the schema object that you have done (that has the schema you want to apply when adding entries in the collection specified in the 1st parameter)
+module.exports = Flight;
