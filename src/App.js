bdcf5fcf61a170f81e68ae4c4fa87d29cd2cab82
AdhamElsheikh <adhamehabmessi@gmail.com>
2021-11-01 22:55:21 +0200

    Searching Flights using the field's value (still not working)

--- src/App.js
+++ src/App.js
@@ -31,6 +31,37 @@ app.get("/newFlight",(req,res)=>{
 });
   me.save();  });
 
+  app.get("/SearchFlight",(req,res)=>{
+    const readline1 = require('readline').createInterface({
+      input: process.stdin,
+    })
+    const readline2 = require('readline').createInterface({
+      input: process.stdin,
+    })
+    const readline3 = require('readline').createInterface({
+      input: process.stdin,
+    })
+    
+      flight.find({From:readline1},{To:readline2},{Available_Seats_on_Flight:readline3},function(err,item)
+      {
+        if(err){
+          res.status(200).send("error");
+
+        }
+        else{
+          res.status.apply(200).send(item);
+          res.status.apply(200).send("\n");
+        }
+
+        })
+      
+    
+      
+
+
+  })
+
+
 
 // Starting server
 app.listen(port, () => {
