ee6c0a47c7edff9d81e6ccd3ca6f7d6becec7f66
Shorook1 <Shorook.soliman@student.guc.edu.eg>
2021-12-04 16:36:16 +0200

    (Mayar) Flight Summary

--- frontend/src/components/MyFlights.jsx
+++ frontend/src/components/MyFlights.jsx
@@ -3,18 +3,24 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 
-
 import { Route, Redirect, useLocation } from "react-router-dom";
+
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal, Button } from 'react-bootstrap';
+require("react-bootstrap/ModalHeader")
+
+
+
 
 
 var flightSelected = 0;
 
-function SendMail(details, From, To)
-{
-    
+function SendMail(details, From, To) {
 
-      const article = { details: details, From:From, To:To };
-      axios.post('http://localhost:8000/SendCancelEmail', article)
+
+    const article = { details: details, From: From, To: To };
+    axios.post('http://localhost:8000/SendCancelEmail', article)
 }
 
 function UpdateFlight(idOfFlight) {
@@ -41,21 +47,27 @@ function handleChange(e, id) {
 function MyFlights() {
 
 
+
     const location = useLocation();
     const history = useHistory();
+
+    const [show, setShow] = useState(false);
+    const handleShow = () => setShow(true);
+    const handleClose = () => setShow(false);
+
 
     const deleteFlight = (event, id, details, from, to) => {
 
         if (isChecked && idDeleted == id) {
             axios.delete(`http://localhost:3000/cancel/${id}`)
-           
-             var x = location.state.email;
+
+            var x = location.state.email;
             event.preventDefault();
             history.push({
-            pathname: '/MyFlights',
-            state: { email: x }
-        });
-        SendMail(details, from, to);
+                pathname: '/MyFlights',
+                state: { email: x }
+            });
+            SendMail(details, from, to);
         }
     };
 
@@ -103,12 +115,11 @@ function MyFlights() {
     )
 
     function GetFlight(FlightNumber) {
-        var From;
-        var To;
+
         var element = depFlights.find(({ Flight_No }) => Flight_No === FlightNumber);
         for (let i = 0; i < depFlights.length; i++) {
             if (depFlights[i].Flight_No == FlightNumber)
-                return [depFlights[i].From, depFlights[i].To]
+                return depFlights[i]
 
         }
         return [];
@@ -124,15 +135,11 @@ function MyFlights() {
             <div>
                 <br />
                 <p>
-                    Booking No.: {flight.BookingNo}  <br /> Cabin: {flight.Cabin}  <br /> Adult Seats: {flight.AdultSeats} |  Children Seats: {flight.ChildrenSeats}
+                    Booking No.: {flight.BookingNo}
                     <br></br>
-                    Departure Flight From : {GetFlight(flight.DepartureFlightNo)[0]}  ||  To: {GetFlight(flight.DepartureFlightNo)[1]}
+                    Departure Flight From : {GetFlight(flight.DepartureFlightNo).From}  ||  To: {GetFlight(flight.DepartureFlightNo).To}
                     <br />
-                    Return Flight From : {GetFlight(flight.ReturnFlightNo)[0]}   || To: {GetFlight(flight.ReturnFlightNo)[1]}
-                    <br />
-                    Price: {flight.Price}<br></br>
-                    Created At ({flight.createdAt}) | Updated At ({flight.updatedAt})
-                    <br></br>
+                    Return Flight From : {GetFlight(flight.ReturnFlightNo).From}   || To: {GetFlight(flight.ReturnFlightNo).To}
                 </p>
                 <label>
                     Are you sure you want to cancel this flight?
@@ -144,16 +151,53 @@ function MyFlights() {
                 </label>
                 <br></br>
 
-                <button
-                    id="removeBtn1"
-                    onClick={(e) => {
-                        deleteFlight(e, flight._id, flight, GetFlight(flight.DepartureFlightNo), GetFlight(flight.ReturnFlightNo));
-                    }}
-                >
-                    Cancel </button>
 
 
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Flight Number {flight.BookingNo} Details</Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body> Booking No.: {flight.BookingNo}<br />  Cabin: {flight.Cabin}  <br /> Adult Seats: {flight.AdultSeats} |  Children Seats: {flight.ChildrenSeats}
+
+                        <br />Price: {flight.Price}<br></br>
+                        <br />
+                        <h5>Departure Flight</h5>
+                        Flight Date: {GetFlight(flight.DepartureFlightNo).FlightDate}<br></br>
+                        Departure Flight From : {GetFlight(flight.DepartureFlightNo).From}  ||  To: {GetFlight(flight.DepartureFlightNo).To}<br />
+                        Departure Time: {GetFlight(flight.DepartureFlightNo).Departure}<br></br>
+                        Arrival Time: {GetFlight(flight.DepartureFlightNo).Arrival}<br></br>
+                        <br />
+
+                        <br />
+                        <h5>Return Flight</h5>
+                        Flight Date: {GetFlight(flight.ReturnFlightNo).FlightDate}<br></br>
+                        Return Flight From : {GetFlight(flight.ReturnFlightNo).From}   || To: {GetFlight(flight.ReturnFlightNo).To}<br />
+                        Departure Time: {GetFlight(flight.ReturnFlightNo).Departure}<br></br>
+                        Arrival Time: {GetFlight(flight.ReturnFlightNo).Arrival}<br></br>
+                        <br />
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <button variant="secondary" onClick={handleClose}>
+                            Close
+                        </button>
+                    </Modal.Footer>
+                </Modal>
+
+                <div  >
+                    <button style={{marginRight : '20px'}}  onClick={handleShow}>
+                        Views Details</button>
+
+                    <button
+                        id="removeBtn1"
+                        onClick={(e) => {
+                            deleteFlight(e, flight._id, flight, GetFlight(flight.DepartureFlightNo), GetFlight(flight.ReturnFlightNo));
+                        }}
+                    >
+                        Cancel </button>
+                </div>
             </div>
+
         )}
     </div>
 
