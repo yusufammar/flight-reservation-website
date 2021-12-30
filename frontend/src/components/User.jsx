f73ab5bde17a8ad7250ac6806162e6b7807aa825
hossammeligy <ho.elmeligy100@gmail.com>
2021-12-30 05:48:26 +0200

    added some Material to landing page

--- frontend/src/components/User.jsx
+++ frontend/src/components/User.jsx
@@ -32,6 +32,21 @@ import Box from '@mui/material/Box';
 import ManIcon from '@mui/icons-material/Man';
 import ChildCareIcon from '@mui/icons-material/ChildCare';
 
+<<<<<<< Updated upstream:frontend/src/components/User.jsx
+=======
+import NavBar from './Helper/NavBar';                     // rendering in return statement (responsible for session checking & returning of current user email)
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+import ListSubheader from '@mui/material/ListSubheader';
+import IconButton from '@mui/material/IconButton';
+import InfoIcon from '@mui/icons-material/Info';
+import ModalUnstyled from '@mui/base/ModalUnstyled';
+import { styled} from '@mui/system';
+
+
+
+>>>>>>> Stashed changes:frontend/src/components/LandingPage.jsx
 function User(){      //for USER & GUEST
 
    /*  
@@ -143,22 +158,111 @@ const options = [ "LAX (Los Angeles International Airport, California, USA)",
 
 var xxx=["x"];
 
+const itemData = [
+  {
+    img: 'https://static.saltinourhair.com/wp-content/uploads/2019/03/23140923/cairo-pyramids-giza.jpg',
+    title: 'Cairo',
+    author: '@visitegypt',
+    rows: 2,
+    cols: 2,
+    featured: true,
+  },
+  {
+    img: 'https://previews.123rf.com/images/f11photo/f11photo1806/f11photo180600097/104444908-statue-liberty-and-new-york-city-skyline-at-sunset-in-united-states.jpg',
+    title: 'NewYork',
+    author: '@iluvnyc',
+  },
+  {
+    img: 'https://cdn.londonandpartners.com/-/media/images/london/visit/things-to-do/sightseeing/london-attractions/tower-bridge/thames_copyright_visitlondon_antoinebuchet640x360.jpg?mw=640&hash=27AEBE2D1B7279A196CC1B4548638A9679BE107A',
+    title: 'London',
+    author: '@goodmorninglondon',
+  },
+  {
+    img: 'https://www.discoverwalks.com/blog/wp-content/uploads/2015/08/beautiful-eiffel-big.jpg',
+    title: 'Paris',
+    author: '@explore.paris',
+  }
+];
+
+
+const itemData2 = [
+  {
+    img: 'https://c.files.bbci.co.uk/3407/production/_103591331_gettyimages-874914576.jpg',
+    title: 'Get to the airport at least three hours before your flight.',
+    
+  },
+  {
+    img: 'https://www.moodiedavittreport.com/wp-content/uploads/2020/05/KLM-mouth-mask-cabin-crew-passengers-covid-19-coronavirus.jpg',
+    title: 'Wear the mask and dont forget vacciantion certifacte.',
+    
+  },
+  {
+    img: 'https://t4.ftcdn.net/jpg/03/14/15/07/360_F_314150765_q2DxXZrbll9KQhe8KnVCvvFPnvv2NbM4.jpg',
+    title: 'Make sure of travel requirements.',
+    
+  }
+];
+const StyledModal = styled(ModalUnstyled)`
+  position: fixed;
+  z-index: 1300;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Backdrop = styled('div')`
+  z-index: -1;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.5);
+  -webkit-tap-highlight-color: transparent;
+`;
+
+const style = {
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  p: 2,
+  px: 4,
+  pb: 3,
+};
+const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
 return ( 
-<div id='container2'  className={css` background-color:white; height: 1000px;' `} >
+  
+<div id='container2'  className={css` background-color:white; height: 1000px; `} >
   
    <div name="content">
+<<<<<<< Updated upstream:frontend/src/components/User.jsx
       <div id='container' className={css` color: white; position: absolute; width: 100%; height: 620px; `} style={{backgroundImage: 'url("/wall5.jpg")'}}>
           <Top2/> 
+=======
+      <div id='container' className={css` color: white; position: relative; width: 100%; height: 620px; `} style={{backgroundImage: 'url("/wall5.jpg")'}}>
+          <NavBar state1="landingPage"/> 
+>>>>>>> Stashed changes:frontend/src/components/LandingPage.jsx
           <div class="overlay">       <div class="text">Fly Safe.</div>     </div>
       </div>
+      <br></br>
+      <br></br>
+      <br></br>
      
 <form className={css`
-  box-shadow: 0px 0px 100px 1px lightGray; position: absolute; left: 12%; top:73%; background-color:white; border-radius: 20px; 
+  box-shadow: 0px 0px 100px 1px lightGray; position: relative; left: 12%; top:73%; background-color:white; border-radius: 20px; 
   width: 75%;  padding: 20px; font-family: 'Josefin Sans';  font-size: 10px;`} >  
   
 &nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 
-<div className={css`display:flex;`}>
+<div className={css`display:flex; postion:relative`}>
 
 <Autocomplete  inputValue={from} onInputChange={handleFrom} options={options} sx={{ width: 400 }}
 renderInput={(params) => <TextField  {...params}  label="From" />}  />
@@ -222,6 +326,7 @@ renderInput={(params) => <TextField  {...params}  label="To" />} />
         </Select>
       </FormControl>
 
+
    
       
 
@@ -229,9 +334,94 @@ renderInput={(params) => <TextField  {...params}  label="To" />} />
 class="btn btn-primary btn-lg" type="submit" value='Search Flights' onClick={handleclick1} /> 
 </div>
 </form>  
+<br></br>
+<br></br>
+<br></br>
+<br></br>
 
-</div>
+  <form style={{color:'#1976d2', fontSize:'20px', position:'relative', left:'28%',width:'25%',textAlign:'center'} }>
+
+    <h2 style={{color:'#1976d2', fontSize:'30px', position:'relative', left:"41%", textTransform:'uppercase', fontFamily: 'Josefin Sans'  }}> SAFELY FLIGHTS</h2>
+    <p style={{width:'200%', position:'relative', left:'-10%', fontFamily: 'Josefin Sans'}}>If you’re travelling soon, here are some important tips to help you prepare for your journey.</p>
+
+    <p style={{position:'relative', left:"-25%"}}>____________________________________________________________________________________________</p>
+  
+    <div style={{width:'50', left:'-55%' , position:'relative'}}>
+    <ImageList sx={{ width: 1200, height: 400, }}>
+        <ImageListItem key="Subheader" cols={3}>
+          <ListSubheader component="div"></ListSubheader>
+        </ImageListItem>
+        {itemData2.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              src={`${item.img}?w=248&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              title={item.title}
+             
+            />
+          </ImageListItem>
+        ))}
+    </ImageList>
+       
+        </div>
+     
+  </form>
+
+  <br></br>
+  <br></br>
+  <br></br><br></br><br></br>
+  <form style={{color:'#1976d2', fontSize:'20px', position:'relative', left:'28%',width:'25%',textAlign:'center'} }>
+
+<h2 style={{color:'#1976d2', fontSize:'30px', position:'relative', left:"41%", textTransform:'uppercase', fontFamily: 'Josefin Sans'  }}>Top Destinations</h2>
+<p style={{width:'200%', position:'relative', left:'-10%', fontFamily: 'Josefin Sans'}}>Take a look for the most visited Destinations in 2021. </p>
+<p style={{position:'relative', left:"-25%"}}>____________________________________________________________________________________________</p>
+
+<div style={{width:'50', left:'-00%' , position:'relative'}}>
+<ImageList sx={{ width: 800, height: 800, }}>
+            <ImageListItem key="Subheader" cols={2}>
+              <ListSubheader component="div"></ListSubheader>
+            </ImageListItem>
+            {itemData.map((item) => (
+              <ImageListItem key={item.img}>
+                <img
+                  src={`${item.img}?w=248&fit=crop&auto=format`}
+                  srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  alt={item.title}
+                  loading="lazy"
+                />
+                <ImageListItemBar
+                  title={item.title}
+                  subtitle={item.author}
+                  actionIcon={
+                    <IconButton
+                      sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
+                      aria-label={`info about ${item.title}`}
+                    >
+                      <InfoIcon> onClick{handleOpen} 
+                      
+                      </InfoIcon>
+                                            
+                    </IconButton>
+                  }
+                />
+              </ImageListItem>
+            ))}
+        </ImageList>
+       
+  
+    
    
+    </div>
+ 
+</form>
+  
+</div>
+
+
 </div>
 
 )} 
