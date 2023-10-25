75b7cf6786071b477834954a936cbe005fcc2745
yusufammar <93484250+yusufammar@users.noreply.github.com>
2023-10-25 11:58:42 +0300

    make it better

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ app.use(express.json());
 app.use("/", require("./Routes/Controller"));
 
 
-if (process.env.NODE_ENV === ‘production’ || process.env.NODE_ENV === ‘staging’) {
-  app.use(express.static(‘client/build’));
-  app.get(‘*’, (req, res) => {
-  res.sendFile(path.join(__dirname + ‘/client/build/index.html’));
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
  }
 
